3fc489fcfcb6c946db034ca04781dabf4c1a7f8e
donghohnation <donghoh.han@gmail.com>
2022-05-18 21:15:36 -0700

    pseudo code comments added to JS

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,22 @@
 // Assignment code here
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+  console.log("Hey U clicked a button");
+
+// 1. promt the user for the password criteria//
+//    a. password lengh 8-28
+//    b. lowercase, uppercase, numbers, special characters
+// 2. validate the input
+// 3. generate password based on criteria
+
+
+// 4. display generated password on the page
+  return "Generated Password will show here"; 
+}
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
